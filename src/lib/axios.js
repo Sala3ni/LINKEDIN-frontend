ad1f48005b20c90ab97905df03fc3d85c31ac07b
Sala3ni <salonimishramishra9527@gmail.com>
2025-08-15 22:10:59 +0530

    main change

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.VITE_API_BASE_URL ||  "https://linkedin-backend-1-0kob.onrender.com",
+	baseURL: import.meta.env.VITE_API_BASE_URL ||  "https://linkedin-backend-2-j7pt.onrender.com",
 	headers: {
 		"Content-Type": "application/json",
 	},
